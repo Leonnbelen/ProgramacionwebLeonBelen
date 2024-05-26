d2762da6496f38210d9029a05d48d6a34d4768fa
Leonnbelen <144805631+Leonnbelen@users.noreply.github.com>
2024-05-26 11:18:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+//GOOGLE MAPS============================================================
 function iniciarMap(){
   var coordenadas = {lat: -43.07016246837909, lng: -71.47421877116459};
+
   var map = new google.maps.Map(document.getElementById('mapajs'),{
     zoom: 15,
     center: coordenadas
   });
+
   var marker = new google.maps.Marker({
     position: coordenadas,
     map: map
   });
 }
+
+//FORMULARIO DE RESERVA====================================================
 
 document.getElementById('reservaForm').addEventListener('submit', function(event) {
   event.preventDefault(); 
@@ -28,5 +33,47 @@ document.getElementById('reservaForm').addEventListener('submit', function(event
       alert('La fecha de salida debe ser posterior a la fecha de entrada.');
       return;
   }
+
   alert('Formulario enviado correctamente.');
 });
+
+//BUSCADOR DE CABANAS======================================================================
+
+const cabañas = [
+  { nombre: 'Cabaña Los Andes', url: 'pagina_cabana_1.html' },
+  { nombre: 'Cabaña El Refugio', url: 'pagina_cabana_2.html' },
+  { nombre: 'Cabaña La Montaña', url: 'pagina_cabana_3.html' },
+  { nombre: 'Cabaña El Bosque', url: 'pagina_cabana_4.html' },
+  { nombre: 'Cabaña El Rio', url: 'pagina_cabana_5.html' }
+];
+
+
+function buscarCabanas() {
+  const query = document.getElementById('buscadorInput').value.toLowerCase();
+  const resultados = cabañas.filter(cabana => cabana.nombre.toLowerCase().includes(query));
+  mostrarResultados(resultados);
+}
+
+function mostrarResultados(resultados) {
+  const listaResultados = document.getElementById('resultadoBusqueda');
+  listaResultados.innerHTML = '';
+
+  if (resultados.length > 0) {
+      resultados.forEach(cabana => {
+          const item = document.createElement('li');
+          const enlace = document.createElement('a');
+          enlace.textContent = cabana.nombre;
+          enlace.href = cabana.url;
+          item.appendChild(enlace);
+          listaResultados.appendChild(item);
+      });
+  } else {
+      listaResultados.innerHTML = '<li>No se encontraron cabañas.</li>';
+  }
+}
+// Mostrar todas las cabañas al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+  mostrarResultados(cabañas);
+});
+document.getElementById('buscadorInput').addEventListener('input', buscarCabanas);
+
